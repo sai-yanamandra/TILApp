30f8f7dba45d797223f20971034b4f9ddd22b22e
Sai Yanamandra <sai.yanamandra@sumerusolutions.com>
2018-12-24 12:30:14 +0530

    extracting timestamp successful

--- Sources/App/Controllers/categorycontroller.swift
+++ Sources/App/Controllers/categorycontroller.swift
@@ -22,20 +22,33 @@ struct CategoriesController: RouteCollection {
 //        return Category.query(on: req).all()
 //    }
     
-    func getHandler(_ req: Request) throws -> Future<Category> {
+    func getHandler(_ req: Request) throws -> Future<String> {
         //return try req.parameters.next(Category.self)
         
         // Fetch an HTTP Client instance
         let client = try req.make(Client.self)
         
+        var headers = HTTPHeaders()
+        headers.add(name: "Authorization", value: "Bearer e59a8939c0514bd793d952254c65e7b8")
+        
+        //let headers = HTTPHeaders("Authorization","Bearer e59a8939c0514bd793d952254c65e7b8")
+    
+        let response = client.get("https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=hello&sessionId=12345&timezone=America/New_York", headers: headers)
+        
+        print ("RESPONSE : \(response)")
         // Send an HTTP Request to example.vapor.codes/json over plaintext HTTP
         // Returns `Future<Response>`
-        let response = client.get("https://jsonplaceholder.typicode.com/todos/4")
+        //let response = client.get("https://jsonplaceholder.typicode.com/todos/4")
         
-        // Transforms the `Future<Response>` to `Future<ExampleData>`
-        return try response.flatMap(to: Category.self) { response in
-            return try response.content.decode(Category.self)
+        //response.map(to: String, <#T##callback: (Response) throws -> T##(Response) throws -> T#>)
+        
+        return try response.map(to: String.self) { response -> String in
+           return try response.content.syncGet(at: "timestamp")
         }
+        // Transforms the `Future<Response>` to `Future<ExampleData>`
+//        return try response.flatMap(to: Category.self) { response in
+//            return try response.content.decode(Category.self["id"])
+//        }
         
         // Renders the `ExampleData` into a `View`
         //return try (exampleData)
@@ -43,6 +56,9 @@ struct CategoriesController: RouteCollection {
     }
 }
 
-
+struct MyResponse {
+    
+    var speech: String
+}
 
 extension Category: Parameter {}
--- Sources/App/Models/category.swift
+++ Sources/App/Models/category.swift
@@ -4,14 +4,13 @@ import Vapor
 
 final class Category: Codable {
     var id: Int?
-   // var idd: Int
     var title: String
     var userId: Int
     var completed: Bool
     
     init(id: Int,userId: Int,title: String,completed: Bool) {
         self.title = title
-       // self.idd = id
+        self.id = id
         self.userId = userId
         self.completed = completed
     }
